33246a277b61d442960367a7eb43ab0285b2df4b
Parkseojin <seojin5565@gmail.com>
2023-12-07 20:41:53 +0900

    [feat] : OrderService - 주문취소 메소드

--- src/main/java/jpabook/jpashop/domain/service/OrderService.java
+++ src/main/java/jpabook/jpashop/domain/service/OrderService.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -23,18 +25,18 @@ public class OrderService {
     //주문
     @Transactional
     public Long order(Long memberId, Long itemId, int count){
-        //엔티티 조회
+        // 엔티티 조회
         Member member = memberRepository.findOne(memberId);
         Item item = itemRepository.findOne(itemId);
 
-        //배송정보 생성
+        // 배송정보 생성
         Delivery delivery = new Delivery();
         delivery.setAddress(member.getAddress());
 
         // 주문 상품 생성
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
 
-        //주문을 생성
+        // 주문을 생성
         Order order = Order.createOrder(member, delivery, orderItem);
 
         // 주문을 저장
@@ -45,8 +47,18 @@ public class OrderService {
     }
 
     //취소
+    @Transactional
+    public void cancelOrder(Long orderId){
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문 취소
+        // jpa는 엔티티 바꾸면 알아서 다 바꾸어준다.
+        order.cancel();
+    }
 
     //검색
-
+//    public List<Order> findOrder(OrderSearch orderSearch){
+//        return orderRepository.findAll();
+//    }
 
 }
